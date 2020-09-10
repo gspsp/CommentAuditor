f47b77a87014acd1626e2f21d75d2fc917f30f56
ihesro <2770520782@qq.com>
2020-09-10 11:39:14 +0800

    1.0.4

--- Plugin.php
+++ Plugin.php
@@ -5,7 +5,7 @@
  *
  * @package CommentAuditor
  * @author ihesro
- * @version 1.0.3
+ * @version 1.0.4
  * @link https://index.php/cross.html
  */
 class CommentAuditor_Plugin implements Typecho_Plugin_Interface
@@ -24,25 +24,28 @@ class CommentAuditor_Plugin implements Typecho_Plugin_Interface
   //设置面板
   public static function config(Typecho_Widget_Helper_Form $form)
   {
-    //ReadMe
-    $ReadMe = new Typecho_Widget_Helper_Form_Element_Select('ReadMe', array('无视我,往下看'), '1', 'ReadMe', '<strong style="color: red;"><a href="https://b.nit9.cn/archives/9.html" target="_blank">食用方法</a>-尽情享用吧！</strong>');
-    $form->addInput($ReadMe);
-    //内容检测API配置
-    $TextCheckApi = new Typecho_Widget_Helper_Form_Element_Text('TextCheckApi', null, null, _t('内容检测API:'));
-    $form->addInput($TextCheckApi->addRule('required', _t('不能为空！')));
+    $apiKey = new Typecho_Widget_Helper_Form_Element_Text('apiKey', null, null, _t('apiKey:'));
+    $form->addInput($apiKey->addRule('required', _t('不能为空！')));
+
+    $secretKey = new Typecho_Widget_Helper_Form_Element_Text('secretKey', null, null, _t('secretKey:'));
+    $form->addInput($secretKey->addRule('required', _t('不能为空！')));
   }
   //主函数
   public static function audit($comment, $post)
   {
     //获取配置项
     $opt = Typecho_Widget::widget('Widget_Options')->plugin('CommentAuditor');
-    $res=json_decode(self::fetch($opt->TextCheckApi . $comment['text'] . $comment['author']), true);
+    $check = self::check($comment['text'] . $comment['author'], $opt->apiKey, $opt->secretKey);
+    $res = array(
+      "type" => @$check['conclusionType'] == 1 ? 'safe' : 'evil',
+      "reason" => @isset($check['data'][0]['msg']) ? $check['data'][0]['msg'] : ''
+    );
     if ($res['type'] == 'evil') {
       $comment['text'] .= "
         <font style='display: inline-block;box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);margin:20px 0px;padding:15px;border-radius:5px;font-size:14px;color:#000000;'>
-          您的昵称或评论内容
-          &nbsp;*<font style='border-bottom: #C7254E 1.5px solid;'>".$res['reason']."</font>*&nbsp;
-          &nbsp;已丢给博主复核。——<a href='https://b.nit9.cn/archives/9.html' target='_blank'>CommentAuditor</a>
+          因昵称或评论内容
+          &nbsp;*<font style='border-bottom: #C7254E 1.5px solid;'>" . $res['reason'] . "</font>*&nbsp;
+          &nbsp;进入博主复核。——<a href='https://b.nit9.cn/archives/9.html' target='_blank'>CommentAuditor</a>
         </font>
       ";
       $comment['status'] = 'waiting';
@@ -53,22 +56,78 @@ class CommentAuditor_Plugin implements Typecho_Plugin_Interface
   }
 
 
-  /**function */
-  public static function fetch($url)
-  {
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_USERAGENT, "Dalvik/1.6.0 (Linux; U; Android 4.1.4; DROID RAZR HD Build/9.8.1Q-62_VQW_MR-2)");
-    curl_setopt($ch, CURLOPT_REFERER, "-");
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-    $str = curl_exec($ch);
-    curl_close($ch);
-    return $str;
-  }
+
   //华丽配置面板
   public static function personalConfig(Typecho_Widget_Helper_Form $form)
   {
+  }
+
+  //以下代码为百度内容审核api
+  protected function request($url, $data, $apiKey, $secretKey)
+  {
+    $params = array();
+    $authObj = self::auth($apiKey, $secretKey);
+    $params['access_token'] = $authObj['access_token'];
+    $params['aipSdk'] = 'php';
+    $params['aipSdkVersion'] = '2_2_17';
+    $response = self::baiduRequest($url . "?" . http_build_query($params), $data, 1);
+    $obj = json_decode($response['content'], true);
+    return $obj;
+  }
+
+  public function auth($apiKey, $secretKey)
+  {
+    $response = self::baiduRequest(
+      'https://aip.baidubce.com/oauth/2.0/token',
+      array(
+        'grant_type' => 'client_credentials',
+        'client_id' => $apiKey,
+        'client_secret' => $secretKey,
+      )
+    );
+    $obj = json_decode($response['content'], true);
+    return $obj;
+  }
+
+  private function baiduRequest($url, $params = "", $ispost = 0)
+  {
+    $params = is_array($params) ? http_build_query($params) : $params;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    if ($ispost) {
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+      curl_setopt($ch, CURLOPT_URL, $url);
+    } else {
+      if ($params) {
+        curl_setopt($ch, CURLOPT_URL, $url . '?' . $params);
+      } else {
+        curl_setopt($ch, CURLOPT_URL, $url);
+      }
+    }
+    $response = curl_exec($ch);
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if ($code === 0) {
+      throw new \Exception(curl_error($ch));
+    }
+
+    curl_close($ch);
+    return array(
+      'code' => $code,
+      'content' => $response,
+    );
+  }
+
+  public function check($message, $apiKey, $secretKey)
+  {
+    $data = array();
+    $data['text'] = $message;
+    return self::request('https://aip.baidubce.com/rest/2.0/solution/v1/text_censor/v2/user_defined', $data, $apiKey, $secretKey);
   }
 }
